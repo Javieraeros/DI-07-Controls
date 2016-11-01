42e6a34a9e0ec04b8c33336ee08f61eaa0bb5f09
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-11-01 03:19:45 +0100

    Faltan archivos tochos

---
+++ Ejercicio ProgressBar/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Ejercicio_ProgressBar
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Picker_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".docx"); //Necesario para que no pete.
+
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                this.Ruta.Text =folder.Path;
+            }
+            else
+            {
+                this.Ruta.Text = "Operation cancelled.";
+            }
+        }
+
+        private async void Prueba_Click(object sender, RoutedEventArgs e)
+        {
+            //https://msdn.microsoft.com/es-es/library/cc148994.aspx
+
+            //https://blogs.msdn.microsoft.com/metroapps/2012/07/15/access-your-application-assets-folder/
+
+            string sourcePath = @"Ficheros";
+            string targetPath =@Ruta.Text;
+
+            //Esto nos guarda en la carpeta de proyecto bin/x86/debux/appX (no es la mejor solución del mundo, explicar)
+            StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            
+            //Seleccionamos el origen y el destino de los ficheros
+            StorageFolder origen = await InstallationFolder.GetFolderAsync(sourcePath);
+            StorageFolder destino = await StorageFolder.GetFolderFromPathAsync(targetPath);
+
+            //leemos todos los ficheros que hay en el origen
+            IReadOnlyList<StorageFile> ficheros =await origen.GetFilesAsync();
+
+            foreach(StorageFile sf in ficheros)
+            {
+                await sf.CopyAsync(destino);
+                /*Puesto que hay 10 ficheros y todos ocupan lo mismo, se le supone el mismo tiempo
+                *Un 10% del total, que en este caso es 10%
+                */
+                Barrita.Value += 0.1*Barrita.Maximum;
+            }
+        }
+    }
+}
---
+++ Prueba/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fichero = "Prueba.txt";
+            string sourcePath = @"C:\Users\Pajar\Documents\GitHub\DI-07-Controls\Ejercicio ProgressBar\Ficheros";
+            string targetPath = @"C:\Users\Pajar\Desktop\Prueba";
+            string fileName, destFile;
+            string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+            // Copy the files and overwrite destination files if they already exist.
+            foreach (string s in files)
+            {
+                // Use static Path methods to extract only the file name from the path.
+                fileName = System.IO.Path.GetFileName(s);
+                destFile = System.IO.Path.Combine(targetPath, fileName);
+                System.IO.File.Copy(s, destFile, true);
+                //Barrita.Value+= 10;
+            }
+            
+        }
+    }
+}
